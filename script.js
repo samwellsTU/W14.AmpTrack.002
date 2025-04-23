13010614ef7863115335b1cb6cc8f877917e4160
samwellsTU <111082260+samwellsTU@users.noreply.github.com>
2025-04-23 13:52:03 -0400

    4/23 inlcass

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const myCtx = new AudioContext();
-const analyser = myCtx.createAnalyser()
-analyser.fftSize = 1024
+const analyser = myCtx.createAnalyser();
+analyser.fftSize = 1024;
 
-const saw = myCtx.createOscillator()
-saw.type = "sawtooth"
+const saw = myCtx.createOscillator();
+saw.type = "sawtooth";
 saw.frequency.value = 100;
 
-const filter = myCtx.createBiquadFilter()
-filter.type = "lowpass"
+const filter = myCtx.createBiquadFilter();
+filter.type = "lowpass";
 filter.q = 50;
 filter.frequency.value = 100;
 
-saw.connect(filter)
-filter.connect(myCtx.destination)
+saw.connect(filter);
+filter.connect(myCtx.destination);
 
+const ampTrack = function () {
+  const data = new Float32Array(analyser.fftSize);
+  analyser.getFloatTimeDomainData(data);
+  console.log(data);
+  requestAnimationFrame(ampTrack);
+};
 
-
-
+document.getElementById("start").addEventListener("click", async () => {
+  myCtx.resume();
+  saw.start();
+  const micStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  const micSource = myCtx.createMediaStreamSource(micStream);
+  micSource.connect(analyser);
+  ampTrack();
+});
