73825bb32a4c74d8856ae160cecbfa4bc10d24c5
samwellsTU <111082260+samwellsTU@users.noreply.github.com>
2025-04-25 13:47:37 -0400

    4/25 in-class

--- script.js
+++ script.js
@@ -14,10 +14,48 @@ filter.frequency.value = 100;
 saw.connect(filter);
 filter.connect(myCtx.destination);
 
+const a2db = function (amp) {
+  return 20 * Math.log10(amp);
+};
+
+let runningAvg = new Array(10);
+runningAvg.fill(0);
+
+const dataSmooth = function (newDatum) {
+  let sum = 0;
+  runningAvg.shift();
+  runningAvg.push(newDatum);
+  runningAvg.forEach((dataPoint) => {
+    sum += dataPoint;
+  });
+  let mean = sum / runningAvg.length;
+  return mean;
+};
+
+const rms = function (sampleArray) {
+  let sum = 0;
+  sampleArray.forEach((indvSamp) => {
+    sum += indvSamp * indvSamp;
+  });
+
+  let mean = sum / sampleArray.length;
+  return Math.sqrt(mean);
+};
+
 const ampTrack = function () {
   const data = new Float32Array(analyser.fftSize);
   analyser.getFloatTimeDomainData(data);
-  console.log(data);
+  let myRMS = rms(data);
+  let myDB = a2db(myRMS);
+  let smoothedDB = dataSmooth(myDB);
+  let cutoff = (smoothedDB - -80) * (4900 / 90) + 100;
+  if (cutoff != -Infinity) {
+    filter.frequency.linearRampToValueAtTime(cutoff, myCtx.currentTime + 0.1);
+  }
+
+  document.getElementById("dBDisp").innerText = `${smoothedDB.toFixed(
+    2
+  )} dB FS`;
   requestAnimationFrame(ampTrack);
 };
 
